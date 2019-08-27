223720c3135c10fd4a2368e89ae1ca20513565ca
jliu14 <jean.liu@morningstar.com>
2019-08-27 14:11:13 +0800

    optimize table div and sort

--- js/gridApp.js
+++ js/gridApp.js
@@ -9,8 +9,7 @@
             var width = opt.width;
             var height= opt.height;
             var showRowNumber = opt.showRowNumber==null?false:opt.showRowNumber;
-            var isAscSort =true;
-
+            
             if(container == null){
                 alert("container is null");
                 return false;
@@ -28,6 +27,9 @@
 
             var view = new View(container,data,columns,width,height,showRowNumber);
             view.init();
+            view.onHeaderClick=function(headerId,isAscSort){
+                board.sort(headerId,isAscSort);
+            }
 
 
             var board = new Grid(data,columns);
@@ -37,45 +39,8 @@
                 view.sort();
             }
 
-            $(".tg-cell-header").click(function(e){
-                
-                var headerText = $(e.target).text();
-                var headerId=null;
-                if(headerText !=null && headerText.length >0){
-                    //get column id by text
-                    for(var j in columns){
-                        var item = columns[j];
-
-                        if(item.name === headerText){
-                            headerId = item.id;
-                            break;
-                        }
-                    }
-
-                    //sort by headerId
-                    this.isAscSort=!this.isAscSort;
-                    $(e.target).addClass('select-header').siblings().removeClass('select-header');
-
-                    if(this.isAscSort){
-                        $(e.target).addClass('toAsc').removeClass('toDesc');
-                        $(e.target).siblings().removeClass('toAsc').removeClass('toDesc');
-                    }else{
-                        $(e.target).addClass('toDesc').removeClass('toAsc');
-                        $(e.target).siblings().removeClass('toAsc').removeClass('toDesc');
-                    }
-
-                    board.sort(headerId,this.isAscSort);
-
-
-                }else{
-                    alert("can not find header");
-                }
-            })
-
-            $(".tg-cell").click(function(e){
-                $(".tg-cell").removeClass('select-cell');
-                $(e.target).addClass('select-cell');
-            })
+        
+            
 
         }
 
--- js/view.js
+++ js/view.js
@@ -3,39 +3,46 @@
     function(window,document,$){
 
         function View(container,data,columns,width,height,showRowNumber){
+            var self=this;
             
-            this.container =container;
-            this.data = data;
-            this.columns =columns;
-            this.width = width;
-            this.height= height;
-            this.showRowNumber = showRowNumber;
-            this.grid=null;
+            self.container =container;
+            self.data = data;
+            self.columns =columns;
+            self.width = width;
+            self.height= height;
+            self.showRowNumber = showRowNumber;
+            self.grid=null;
+            self.isAscSort =true;
         }
 
         View.prototype ={
             init:function(){
+                var self=this;
                 var gridObj = document.createElement("div");
-                this.grid= gridObj;
+                self.grid= gridObj;
 				gridObj.classList.add('tg');
-                gridObj.style.width= this.width + "px";
-                //gridObj.style.height= this.height + "px";
+                gridObj.style.width= self.width + "px";
+                //gridObj.style.height= self.height + "px";
+                self.container.appendChild(self.grid);
                 
-                this.createHeader();
-                this.createBody();
-                this.container.appendChild(gridObj);
+                self.createHeader();
+                self.createBody();
+                
             },
             createHeader:function(){
-                var columnInfo = this.columns;
+                var self=this;
+                var columnInfo = self.columns;
 
                  //remove old node
-                var code = document.getElementsByClassName("tg-row-header");
-                for(var i = code.length - 1; i >= 0; i--){ 
-                     code[i].parentNode.removeChild(code[i]); 
-                }
+                $(self.grid).children(".tg-div-header").remove();
+
+                var headerDiv = document.createElement('div');
+                headerDiv.classList.add('tg-div-header');
 
                 var header = document.createElement('div');
                 header.classList.add('tg-row-header');
+
+                headerDiv.appendChild(header);
 
                 for(var j=0;j<columnInfo.length;j++){
                     var columnInfoItem = columnInfo[j];
@@ -45,17 +52,54 @@
                     header.appendChild(cell);
                 }
 
-                this.grid.appendChild(header);
+                self.grid.appendChild(headerDiv);
+                
+
+                $(".tg-cell-header").click(function(e){
+                    var headerText = $(e.target).text();
+                    var headerId=null;
+                    var columns = self.columns;
+                    
+                    if(headerText !=null && headerText.length >0){
+                        //get column id by text
+                        for(var j in columns){
+                            var item = columns[j];
+    
+                            if(item.name === headerText){
+                                headerId = item.id;
+                                break;
+                            }
+                        }
+    
+                        //sort by headerId
+                        self.isAscSort=!self.isAscSort;
+                        $(e.target).addClass('select-header').siblings().removeClass('select-header');
+    
+                        if(self.isAscSort){
+                            $(e.target).addClass('toAsc').removeClass('toDesc');
+                            $(e.target).siblings().removeClass('toAsc').removeClass('toDesc');
+                        }else{
+                            $(e.target).addClass('toDesc').removeClass('toAsc');
+                            $(e.target).siblings().removeClass('toAsc').removeClass('toDesc');
+                        }
+    
+                        self.onHeaderClick(headerId,self.isAscSort);
+
+                    }else{
+                        alert("can not find header");
+                    }
+                })
             },
             createBody:function(){
-                var dataList = this.data;
-                var columnInfo = this.columns;
+                var self=this;
+                var dataList = self.data;
+                var columnInfo = self.columns;
 
                 //remove old node
-                var code = document.getElementsByClassName("tg-row");
-                for(var i = code.length - 1; i >= 0; i--){ 
-                    code[i].parentNode.removeChild(code[i]); 
-                }
+                $(self.grid).children(".tg-div-body").remove();
+
+                var bodyDiv = document.createElement('div');
+                bodyDiv.classList.add('tg-div-body');
 
                 //create new node
                 for(var k=0;k<dataList.length;k++){
@@ -66,21 +110,62 @@
                         var columnInfoItemId = columnInfo[j].id;
                         var cell = document.createElement("div");
                         cell.classList.add('tg-cell');
+                        cell.setAttribute('columnid',columnInfoItemId);
+
                         cell.innerText=dataList[k][columnInfoItemId]
                         row.appendChild(cell);
                     }
 
-                    this.grid.appendChild(row);
+                    bodyDiv.appendChild(row);
                 }
 
+                self.grid.appendChild(bodyDiv);
+        
+
+                //bind click
+                $(".tg-cell").click(function(e){
+                    $(".tg-cell").removeClass('select-cell');
+                    $(e.target).addClass('select-cell');
+
+                    //get current column info
+                    // var columnId = self.getAttribute('columnid');
+                    // var currentColumnInfo = self.getColumnInfoById(columnId);
+
+                    // var isReadOnly= currentColumnInfo.readOnly==null?false:currentColumnInfo.readOnly;
+                    // var renderer=currentColumnInfo.renderer==null?null:currentColumnInfo.renderer;
+                    // var dataType=currentColumnInfo.type ==null?'text':currentColumnInfo.type;
+
+                    //priority:  readOnly > renderer > type
+
+                    // if(isReadOnly){
+                    //     return false;
+                    // }
+
+
+                })
                 
             },
             createFooter:function(){
 
             },
+            onHeaderClick:function(headerId,isAscSort){
+
+            },
             sort:function(){
-                this.createBody();
-                this.container.appendChild(this.grid);
+                var self=this;
+                self.createBody();
+                self.container.appendChild(self.grid);
+            },
+            getColumnInfoById:function(columnId){
+                var self=this;
+                var columnObj= new Object();
+                for(var j=0;j<self.columns.length;j++){
+                    if(self.columns[j].id === columnId){
+                        columnObj= self.columns[j];
+                    }
+                }
+
+                return columnObj;
             }
 
         }
